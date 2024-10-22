1221f9504f466b1baaf81d2e5376fa8645258dd7
AgungHidayanto <hidayantoagung16@gmail.com>
2024-10-22 17:32:32 +0700

    Tampilan

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './monitoring.html' // Ini akan mencakup semua file HTML dalam folder 'src'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
